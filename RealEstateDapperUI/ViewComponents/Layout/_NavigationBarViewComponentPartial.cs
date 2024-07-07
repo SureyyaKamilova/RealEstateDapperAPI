6667821e77bc68d0679dfd096312ee617cfc3ab0
user <user@DESKTOP-DDOO6UB>
2024-07-07 20:30:20 +0400

    Share index.html. Create ViewComponents

---
+++ RealEstateDapperUI/ViewComponents/Layout/_NavigationBarViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstateDapperUI.ViewComponents.Layout
+{
+    public class _NavigationBarViewComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
