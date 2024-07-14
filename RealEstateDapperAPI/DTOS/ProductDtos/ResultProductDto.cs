d42a560dd4b1e08b28877bee6b08d8db49fd7246
user <user@DESKTOP-DDOO6UB>
2024-07-14 14:19:01 +0400

    There are a few changes

--- RealEstateDapperAPI/DTOS/ProductDtos/ResultProductDto.cs
+++ RealEstateDapperAPI/DTOS/ProductDtos/ResultProductDto.cs
@@ -3,7 +3,7 @@
     public class ResultProductDto
     {
         public int ProductId {  get; set; }
-        public string Titlke {  get; set; }
+        public string Title {  get; set; }
         public decimal Price {  get; set; }
         public string City {  get; set; }
         public string District {  get; set; }
--- RealEstateDapperAPI/DTOS/ProductDtos/ResultProductWithCategory.cs
+++ RealEstateDapperAPI/DTOS/ProductDtos/ResultProductWithCategory.cs
@@ -3,7 +3,7 @@
     public class ResultProductWithCategory
     {
         public int ProductId { get; set; }
-        public string Titlke { get; set; }
+        public string Title { get; set; }
         public decimal Price { get; set; }
         public string City { get; set; }
         public string District { get; set; }
