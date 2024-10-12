cca203cc86d2984ccf4fa60cc0cc3400ae78f456
user <user@DESKTOP-DDOO6UB>
2024-10-12 13:42:45 +0400

    CRUD Employee. Fix problems

--- RealEstateDapperUI/Controllers/EmployeeController.cs
+++ RealEstateDapperUI/Controllers/EmployeeController.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using RealEstateDapperUI.DTOS.EmployeeDtos;
 using System.Net.Http;
+using System.Text;
 
 namespace RealEstateDapperUI.Controllers
 {
@@ -23,6 +24,72 @@ namespace RealEstateDapperUI.Controllers
 
             }
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEmployee(CreateEmployeeDto employeeDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData =  JsonConvert.SerializeObject(employeeDto);
+
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44364/api/Employee", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+
+               return RedirectToAction("Index");
+
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:44364/api/Employee/{id}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateEmployee(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:44364/api/Employee/{id}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateEmployeeDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateEmployee(UpdateEmployeeDto updateEmployee)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateEmployee);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            var responseMessage = await client.PutAsync("https://localhost:44364/api/Employee/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+
         }
     }
 }
